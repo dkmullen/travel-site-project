29cc4b70fb05409f2d39e8a36d76daba301da448
Dennis Mullen <dkmullen@gmail.com>
2017-07-11 22:03:12 -0400

    Work on styles and also code organization

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,8 @@ const gulp = require('gulp'),
   postcss = require('gulp-postcss'), // main package to add post-processing to css
   autoprefixer = require('autoprefixer'), // adds vendor prefixes when needed
   cssvars = require('postcss-simple-vars'), // processes vars into valid css
-  nested = require('postcss-nested'); // processes nested css into valid css
+  nested = require('postcss-nested'), // processes nested css into valid css
+  cssImport = require('postcss-import'); // Allows for importing css as modules
 
 // First arg is the name, 2nd is what it does; Default task runs at 'gulp' command
 gulp.task('default', () => {
@@ -18,7 +19,7 @@ gulp.task('html', () => {
 
 gulp.task('styles', () => {
   return gulp.src('./app/assets/styles/styles.css') // Get this file...
-    .pipe(postcss([cssvars, nested, autoprefixer])) // ...run it thru these...
+    .pipe(postcss([cssImport, cssvars, nested, autoprefixer])) // ...run it thru these...
     .pipe(gulp.dest('./app/temp/styles')); // ...then put it here.
 });
 
