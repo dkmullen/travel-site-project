47365c1eeac6ab601016ec490da641fcc8bca02a
Dennis Mullen <dkmullen@gmail.com>
2017-07-15 11:17:39 -0400

    Added reusuable blocks of CSS, other styling

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -5,10 +5,15 @@ const gulp = require('gulp'),
   autoprefixer = require('autoprefixer'), // adds vendor prefixes when needed
   cssvars = require('postcss-simple-vars'), // processes vars into valid css
   nested = require('postcss-nested'), // processes nested css into valid css
-  cssImport = require('postcss-import'); // Allows for importing css as modules
+  cssImport = require('postcss-import'), // Allows for importing css as modules
+  mixins = require('postcss-mixins'); // mixins are reusable bits of code
 
   gulp.task('styles', () => {
     return gulp.src('./app/assets/styles/styles.css') // Get this file...
-      .pipe(postcss([cssImport, cssvars, nested, autoprefixer])) // ...run it thru these...
+      .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer])) // ...run it thru these...
+      .on('error', function(errorInfo) { // Crashes on fat-arrow function
+        console.log(errorInfo.toString());
+        this.emit('end'); // End this task gracefully on error
+      })
       .pipe(gulp.dest('./app/temp/styles')); // ...then put it here.
   });
