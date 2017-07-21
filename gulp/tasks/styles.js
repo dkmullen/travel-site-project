0d1d44e638fa3816037906c0208bfb7fd7182036
Dennis Mullen <dkmullen@gmail.com>
2017-07-21 19:28:40 -0400

    Finished styling header

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -6,11 +6,12 @@ const gulp = require('gulp'),
   cssvars = require('postcss-simple-vars'), // processes vars into valid css
   nested = require('postcss-nested'), // processes nested css into valid css
   cssImport = require('postcss-import'), // Allows for importing css as modules
-  mixins = require('postcss-mixins'); // mixins are reusable bits of code
+  mixins = require('postcss-mixins'), // mixins are reusable bits of code
+  hexrgba = require('postcss-hexrgba'); // allows use of hex variables w/in rgba brackets
 
   gulp.task('styles', () => {
     return gulp.src('./app/assets/styles/styles.css') // Get this file...
-      .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer])) // ...run it thru these...
+      .pipe(postcss([cssImport, mixins, cssvars, nested, hexrgba, autoprefixer])) // ...run it thru these...
       .on('error', function(errorInfo) { // Crashes on fat-arrow function
         console.log(errorInfo.toString());
         this.emit('end'); // End this task gracefully on error
