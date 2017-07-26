9ca0b03d70bc4699cfefb65de7675e85ad74f698
Dennis Mullen <dkmullen@gmail.com>
2017-07-26 18:11:33 -0400

    Made some animations for features and testimonies

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,8 +1,12 @@
 /*jshint esversion: 6 */
 
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
+import $ from 'jquery';
 
 const mobileMenu = new MobileMenu();
+new RevealOnScroll($('.feature-item'), '85%');
+new RevealOnScroll($('.testimonial'), '60%');
 
 /* In this file, webpack calls jquery from an npm install; no need to call it
 in the browser */
