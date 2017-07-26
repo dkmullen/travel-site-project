7fbc6ff2e05f8346e99fae0bee5da7e39795ac27
Dennis Mullen <dkmullen@gmail.com>
2017-07-25 20:55:01 -0400

    Style work on mobile menu

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -4,6 +4,7 @@ import $ from 'jquery'; // brings jq in via NPM rather than in the browser
 
 class MobileMenu {
   constructor() {
+    this.siteHeader = $('.site-header');
     this.menuIcon = $('.site-header__menu-icon');
     this.menuContent = $('.site-header__menu-content');
     this.events();
@@ -16,6 +17,8 @@ class MobileMenu {
 
   toggleTheMenu() {
     this.menuContent.toggleClass('site-header__menu-content--is-visible');
+    this.siteHeader.toggleClass('site-header--is-expanded');
+    this.menuIcon.toggleClass('site-header__menu-icon--close-x');
   }
 }
 
