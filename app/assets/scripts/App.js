aacb5e71c2ba0ea9d7e1fa1f7a9671302a584991
Dennis Mullen <dkmullen@gmail.com>
2017-07-28 17:56:57 -0400

    Completed modal overlay

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -4,11 +4,13 @@ import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import $ from 'jquery';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 
 const mobileMenu = new MobileMenu();
 new RevealOnScroll($('.feature-item'), '85%');
 new RevealOnScroll($('.testimonial'), '60%');
 const stickyHeader = new StickyHeader();
+const modal = new Modal();
 
 /* In this file, webpack calls jquery from an npm install; no need to call it
 in the browser */
